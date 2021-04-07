9ff8cfb7bc9e006c1759db9ab2088b69af13b827
James Reid <jreidmke@gmail.com>
2021-04-07 14:48:38 -0500

    plus one

--- probs.js
+++ probs.js
@@ -284,6 +284,22 @@ function climbStairs(n, memo={1: 1, 2: 2}) {
     return memo[n]
 };
 
+function plusOne(digits, lastDigit=digits[digits.length-1]) {
+    for(let i = digits.length - 1; i >= 0; i--) {
+        digits[i]++;
+        if(digits[i] > 9) {
+            digits[i] = 0
+        } else {
+            return digits
+        }
+    };
+    digits.unshift(1);
+    return digits
+}
+
+plusOne([9])
+
+
 /**Come Back Not Done Trouble Hard Incomplete */
 
 //Reverse a Linked List => https://medium.com/outco/reversing-a-linked-list-easy-as-1-2-3-560fbffe2088
