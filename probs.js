137c9bd9268b51fa830e57936ad7fe70675fac45
James Reid <jreidmke@gmail.com>
2021-04-08 14:08:53 -0500

    pascal triangle

--- probs.js
+++ probs.js
@@ -563,8 +563,19 @@ const KEYBOARD = {
       }
   }
   
-
-
+//pascal triangle
+  function generate(numRows, tri=[[1], [1, 1]]) {
+    if(numRows===1) return [[1]];
+    if(tri.length===numRows) return tri;
+    let lastRow = tri[tri.length - 1];
+    let subArr = [1];
+    for(let i=1; i < lastRow.length; i++) {
+            subArr.push(lastRow[i-1] + lastRow[i])
+    }
+    subArr.push(1);
+    tri.push(subArr);
+    return generate(numRows, tri);
+};
 
 
 
