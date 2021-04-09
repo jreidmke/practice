77f11fc772a8f161902750d231330bee0f78539e
James Reid <jreidmke@gmail.com>
2021-04-09 13:52:06 -0500

    intersecting linked lists

--- probs.js
+++ probs.js
@@ -845,3 +845,15 @@ var merge = function(nums1, m, nums2, n) {
     }
     return nums1
   };
+
+//Intersecting linked list
+  var getIntersectionNode = function(headA, headB) {
+    if(!headA && !headB) return null;
+    let currA = headA;
+    let currB = headB;
+    while(currA !== currB) {
+        currA = currA == null ? headB : currA.next;
+        currB = currB == null ? headA : currB.next;
+    };
+    return currA;
+};
